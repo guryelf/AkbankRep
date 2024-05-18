ccc0af82aef729b36936f8b5be0cb49d8ac07df2
FG <69600609+guryelf@users.noreply.github.com>
2024-05-18 21:52:43 +0300

    Update translator.py

--- akbank/modules/translator.py
+++ akbank/modules/translator.py
@@ -7,11 +7,18 @@ class CategoryTranslator:
         self.category_mapping = self.load_mapping()
 
     def load_mapping(self):
-        with open(self.translations_file, 'r', encoding='utf-8') as f:
-            translations = json.load(f)
-            return translations.get('category_translations', {})
+        try:
+            with open(self.translations_file, 'r', encoding='utf-8') as f:
+                translations = json.load(f)
+                category_mapping = translations.get('category_translations', {})
+                print("Successfully loaded translations:", category_mapping)
+                return category_mapping
+        except Exception as e:
+            print("Error loading translations:", e)
+            return {}
 
     def translate_categories(self, df, column_name):
+        print("Loaded translations:", self.category_mapping)
         df['translated_category'] = df[column_name].map(self.category_mapping.get)
         return df
 
